91d8c4901bc6310c692a8dcd43995908daef3299
liukai <869896679@qq.com>
2018-06-29 13:21:20 +0800

    update

--- thirdCommonModules/getips.py
+++ thirdCommonModules/getips.py
@@ -2,5 +2,20 @@ import requests,json
 ret=requests.get('http://server.zeus.xiaodutv.cn:8900/zeusapi/v1/node/?id=143')
 datas=json.loads(ret.text)
 iplist=''
+ad_odp_bj=0
+ad_odp_gz=0
+odp_ex_bj=0
+odp_ex_gz=0
 for data in datas['data']['machines']:
-    print(data['ip'])
+    if data['hostname'].find('ad-odp') != -1:
+        if data['hostname'].find('bj') != -1:
+            ad_odp_bj+=1
+        elif data['hostname'].find('gz') != -1:
+            ad_odp_gz+=1
+    elif data['hostname'].find('odp-ex') != -1:
+        if data['hostname'].find('bj') != -1:
+            odp_ex_bj+=1
+        elif data['hostname'].find('gz') != -1:
+            odp_ex_gz+=1
+
+print('ad_odp_bj:%s,ad_odp_gz:%s,odp_ex_bj:%s,odp_ex_gz:%s' % (ad_odp_bj,ad_odp_gz,odp_ex_bj,odp_ex_gz))
